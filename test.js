3653a3836cfba58b0991009891764e92c4bae3a4
xiaowoxiaowo <756967944@qq.com>
2020-10-19 14:34:34 +0800

    fix: 测试代码冲突合并

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 // 测试vscode的git功能
 // 这是github的修改
 // 这是test123分支的内容
+function test() {
+  var arr1 = [2, 3, 4];
+  arr1.forEach((item) => {
+    console.log("2222");
+    console.log(item);
+  });
+}
+
+function test1() {
+  var arr2 = [1, 7, 4];
+  arr2.forEach((item) => {
+    console.log(item);
+  });
+}
