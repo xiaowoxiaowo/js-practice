1ecb76cbe4225040082c98ae9233615ebed84d88
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-08-04 18:09:21 +0800

    feat:mianshi

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # js-practice
-<<<<<<< HEAD
 一些js练习,小tip，js方法啊a190122
-=======
 一些js练习,小tip，js 方法
 
->>>>>>> c54ad770281a1ae5c521e9c4b248a21783eef2d8
---
+++ mianshi.js
@@ -0,0 +1,68 @@
+/**
+ * https://mp.weixin.qq.com/s/RLQHkTtlkNmRupMrJGJfKg
+ * tapable
+ * module, chunk, bundle的区别
+ * webpack优化构建速度(开发，生产)
+ * POST和GET的区别
+ * 继承（构造函数继承，原型链继承，最佳继承方式）
+ * TCP和OSI的几层协议
+ * webSocket
+ * https非对称加密，为什么安全
+ * TCP三次握手，SYN，SYN.ACK，ACK；TCP四次握手关闭连接：FIN，ACK/FIN，ACK
+ * 什么是同源策略以及限制
+ * 跨域通信的几种方式：JSONP，Hash，PostMessage，WebSocket，CORS
+ * 什么是DOCTYPE及作用
+ * 重排Reflow
+ * 重绘Repaint
+ * 垃圾回收
+ * 
+ * 闭包数据怎么检查
+ * TCP 丢包阻塞问题
+ * css 选择器权重
+ * 服务端渲染了解吗 和 SPA(单页面应用) 的区别
+ * 如何触发bfc
+ * this指向
+ * 一个异步任务调度器，最多同时执行两个异步任务
+ * 一个网站 SEO 怎么优化
+ * dns 预解析的怎么实现的?
+ * http2的优点
+ * JWT
+ * 浏览器怎么验证https证书的?
+ * cdn缓存
+ * 浏览器缓存配置字段过一遍
+ * 你所知道的白屏原因
+ * IEEE 754 了解吗
+ * TypeScript 高级用法需要再过一遍， 比如Pick 和 Omit
+ * 判断一个字符串最多只删1个字符，是否能成为一个回文字符串
+ * 单向链表输出倒数第 K 个元素
+ * 用正则表达式获取 url 中 query 参数，分割千分数字
+ * 
+ * react hooks 实现原理
+ * 说下输入一个 url 地址的全过程。
+ * 
+ * dom树是怎么解析的
+ * webpack hmr原理，怎么保留之前更改的状态
+ * react-redux
+ * useSelector
+ */
+async function async1() {
+  console.log('async1 start');
+  await async2(); 
+  console.log('async1 end');
+}
+async function async2() {
+  console.log('async2');
+  // 注意：这里如果返回 Promise 的话执行顺序就不一样了
+}
+console.log('script start');
+setTimeout(function() {
+  console.log('setTimeout');
+}, 0)
+async1();
+new Promise(function(resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function() {
+  console.log('promise2');
+});
+console.log('script end');
