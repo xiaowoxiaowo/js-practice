63960b128f8b6427fae96a1e10c85b70e60a61f4
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-08-18 23:29:31 +0800

    *

--- mianshi.js
+++ mianshi.js
@@ -1,9 +1,16 @@
 /**
  * 
- * for循环底层？
  * tcp阻塞
  * react hooks 实现原理
  * webpack hmr原理，怎么保留之前更改的状态
  * react-redux
  * useSelector
+ * sourcemap
+ * console.trace()
+ * webpack loader https://mp.weixin.qq.com/s/5UilDNICyUgK95Ahvdu8MA
+ * commonjs和esm细节 https://mp.weixin.qq.com/s/_VcB8GijWuQ2wgQyk6vrjA
+ * vue3.2响应式优化 https://mp.weixin.qq.com/s/02-6xMskeTMuTwrJ1fkZow
+ * vue3.2优化内容 https://mp.weixin.qq.com/s/nppJSeRmX4PX7y1xIFtdLA
+ * 新的页面核心性能指标 https://mp.weixin.qq.com/s/NYqdeQm5tnQZ1NrgL9vsJQ
+ * 
  */
