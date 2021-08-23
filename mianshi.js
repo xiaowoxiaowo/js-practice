5e304d7f7c4395428e1efe73c2576c5209018fa2
xiaowoxiaowo <756967944@qq.com>
2021-08-24 01:30:35 +0800

    *

--- mianshi.js
+++ mianshi.js
@@ -8,5 +8,12 @@
  * vue3.2优化内容 https://mp.weixin.qq.com/s/nppJSeRmX4PX7y1xIFtdLA
  * vue-route，react-route，keep-alive
  * vue-hot-load，react-hot
- * 面题  https://mp.weixin.qq.com/s/UEsTQjoSbOFh3nv7_TePfg
+ * 手写一个简单redux
+ * redux缺点
+ * react生命周期
+ * fiber相关 https://juejin.cn/post/6844903975112671239
+ * 函数式编程 https://juejin.cn/post/6844903936378273799
+ * webpack5新特性：https://juejin.cn/post/6844904169405415432
+ * webpack 热更新原理: https://juejin.cn/post/6844904008432222215
+ * MVC,MVP,MVVM
  */
---
+++ 手写常用或原生方法/手写async&await.js
@@ -0,0 +1,35 @@
+const getData = () => new Promise(resolve => setTimeout(() => resolve("data"), 1000))
+  
+var test = asyncToGenerator(
+    function* testG() {
+      // await被编译成了yield
+      const data = yield getData()
+      console.log('data: ', data);
+      const data2 = yield getData()
+      console.log('data2: ', data2);
+      return 'success'
+    }
+)
+
+function asyncToGenerator(generatorFunc) {
+	return function() {
+		const gen = generatorFunc.apply(this, arguments)
+		return new Promise((resolve, reject) => {
+			function step(key, arg) {
+				let generatorResult
+				try {
+					generatorResult = gen[key](arg)
+				} catch (error) {
+					return reject(error)
+				}
+				const { value, done } = generatorResult
+				if (done) {
+					return resolve(value)
+				} else {
+					return Promise.resolve(value).then(val => step('next', val), err => step('throw', err))
+				}
+			}
+			step("next")
+		})
+	}
+}
