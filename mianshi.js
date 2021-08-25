9411e1d762a08a1aa18861820650fb7a3358b5bf
xiaowoxiaowo <756967944@qq.com>
2021-08-25 23:36:08 +0800

    8

--- mianshi.js
+++ mianshi.js
@@ -3,17 +3,9 @@
  * react hooks 实现原理
  * webpack hmr原理，怎么保留之前更改的状态 https://mp.weixin.qq.com/s/4fCBeNLhaWJYEcTb8cmakg
  * react-redux
- * useSelector
- * webpack loader https://mp.weixin.qq.com/s/5UilDNICyUgK95Ahvdu8MA
- * vue3.2优化内容 https://mp.weixin.qq.com/s/nppJSeRmX4PX7y1xIFtdLA
  * vue-route，react-route，keep-alive
  * vue-hot-load，react-hot
  * 手写一个简单redux
- * redux缺点
- * react生命周期
  * fiber相关 https://juejin.cn/post/6844903975112671239
- * 函数式编程 https://juejin.cn/post/6844903936378273799
- * webpack5新特性：https://juejin.cn/post/6844904169405415432
  * webpack 热更新原理: https://juejin.cn/post/6844904008432222215
- * MVC,MVP,MVVM
  */
