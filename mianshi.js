d52fa269532915b5b1b80e58a37b8742d3ebecb8
xiaowoxiaowo <756967944@qq.com>
2021-09-02 01:09:39 +0800

    *

--- mianshi.js
+++ mianshi.js
@@ -22,4 +22,10 @@
  * 如果让你设计项目自动设计组件升级，并且安全，你会怎么去设计
  * 接着主要聊业务相关的，看看我对业务的理解，以及一些想法。
  * 业务方怎么拿到反馈的，就是做的这个东西具体有什么用？
+ * 
+ * 文章推荐：手撸组件库之文档工具  https://juejin.cn/post/7002898476388319269?share_token=c0577819-087b-4c2d-9e85-11dac8e868ba
+ * https://www.nowcoder.com/discuss/710552?type=2&channel=-1&source_id=discuss_terminal_discuss_jinghua_nctrack
+ * 
+ * 自己相对于其它竞争者的独特之处
+ * 自己的优缺点
  */
